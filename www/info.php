a2b7d3cae6329539297de92888badd615d3f026a
DAW2 <57674244+cacike69@users.noreply.github.com>
2021-02-24 19:43:07 +0100

    Ejercicio ruben
    Apruebame

---
+++ www/info.php
@@ -0,0 +1,15 @@
+<!-- <?php
+//phpinfo();
+?> -->
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Lucian</title>
+</head>
+<body>
+    <h3>PESADO!!</h3>
+</body>
+</html>
